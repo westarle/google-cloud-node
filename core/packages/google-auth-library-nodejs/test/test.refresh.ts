6c5e4a994ef3b261bbfb4a73da116c224ecac7e1
Wes Tarle <westarle@google.com>
2026-08-16 03:38:58 +0000

    fix(auth): reject non-default universe domains for UserRefreshClient

--- core/packages/google-auth-library-nodejs/test/test.refresh.ts
+++ core/packages/google-auth-library-nodejs/test/test.refresh.ts
@@ -101,6 +101,17 @@ describe('refresh', () => {
     assert.strictEqual(json.refresh_token, refresh._refreshToken);
   });
 
+  it('fromJSON should throw if a non-default universeDomain is provided in the JSON payload', () => {
+    const json = {
+      ...createJSON(),
+      universe_domain: 'my-universe.goog',
+    };
+    const refresh = new UserRefreshClient();
+    assert.throws(() => {
+      refresh.fromJSON(json);
+    }, /refresh is only supported in the default googleapis.com universe domain/);
+  });
+
   it('fromStream should error on null stream', done => {
     const refresh = new UserRefreshClient();
     // Test verifies invalid parameter tests, which requires cast to any.
